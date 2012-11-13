cf9c23639b9046c6cede28e76633c0af9c3194c8
adorsk <adorsk>
2012-11-13 14:07:48 -0500

    hackety way of embedding project page

---
+++ src/custom/themes/sasi_home/node--georefine-project.tpl.php
@@ -0,0 +1,28 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      print render($content);
+    ?>
+    <div class="georefine">
+      <iframe src="<?php print $georefine_project_url ?>" style="width: 100%; height: 800px;">
+      </iframe>
+    </div>
+  </div>
+
+
+  <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <div class="links"><?php print render($content['links']); ?></div>
+    <?php endif; ?>
+
+    <?php print render($content['comments']); ?>
+  </div>
+
+</div>
